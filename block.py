14b422dda25c7b9a7958d685001c5cfb4f359d9a
Seledtsov Daniil <danikseledtsov@gmail.com>
2025-04-02 00:04:55 +0300

    Added function of files

--- block.py
+++ block.py
@@ -1,15 +1,24 @@
 import json
+import os
+
 def write_block(name,amount,to_whom,prev_hash=''):
+    blockchain_dir=os.curdir +'/blocks/'
+    files =os.listdir(blockchain_dir)
+    files=sorted([int(i) for i in files ] )
+
+    last_file=files[-1]
+    filename=str(last_file+1)
+
     data={'name':name,
           'amount':amount,
           'to_whom':to_whom,
           'hash':prev_hash,}
-    with open("test",'w') as file:
+    with open(blockchain_dir+filename,'w') as file:
         json.dump(data,file,indent=4)
 
 
 def main():
-    write_block(name="Ivan",amount=100,to_whom="Ihar")
+    write_block(name="Ivan",amount=100,to_whom="Daniil")
 
 
 
