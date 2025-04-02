25c74966e2d3b24091c8fc686302ee8a833de48e
Seledtsov Daniil <danikseledtsov@gmail.com>
2025-04-02 16:30:03 +0300

    Created blockchain

--- block.py
+++ block.py
@@ -2,31 +2,55 @@ import json
 import os
 import hashlib
 
+
+
+def get_blockchain_dir():
+    return os.curdir + '/blocks/'
+
+def get_sorted_files(blockchain_dir):
+    files = os.listdir(blockchain_dir)
+    files = sorted([int(i) for i in files])
+    return files
+
 def get_hash(filename):
-    blockchain_dir = os.curdir + '/blocks/'
+    blockchain_dir = get_blockchain_dir()
     file=open(blockchain_dir + filename,"rb").read()
     return hashlib.sha256(file).hexdigest()
 
-def write_block(name,amount,to_whom,prev_hash=''):
-    blockchain_dir=os.curdir +'/blocks/'
-    files =os.listdir(blockchain_dir)
-    files=sorted([int(i) for i in files ] )
+def check_integrity():
+    blockchain_dir = get_blockchain_dir()
+    files=get_sorted_files(blockchain_dir)
+    results=[]
+    for file in files[1:]:
+        h=json.load(open(blockchain_dir + str(file)))['hash']
+        prev_file=str(file-1)
+        actual_hash = get_hash(prev_file)
+        if h==actual_hash:
+            res='Ok'
+        else:
+            res='Corrupted'
+        results.append({'block':prev_file,'result':res})
+    return results
 
-    last_file=files[-1]
-    filename=str(last_file+1)
+def write_block(name,amount,to_whom,prev_hash=''):
+    blockchain_dir = get_blockchain_dir()
+    blockchain_dir = get_blockchain_dir()
+    files = get_sorted_files(blockchain_dir)
+
+    prev_file=files[-1]
+    filename=str(prev_file+1)
 
     data={'name':name,
           'amount':amount,
           'to_whom':to_whom,
-          'hash':get_hash(str(last_file))}
+          'hash':get_hash(str(prev_file))}
     with open(blockchain_dir+filename,'w') as file:
         json.dump(data,file,indent=4)
 
 
 def main():
-    write_block(name="Alex",amount=78,to_whom="Milana")
-
-
+    # write_block(name="Alex",amount=78,to_whom="Milana")
+    print(check_integrity())
 
 if __name__=='__main__':
     main()
