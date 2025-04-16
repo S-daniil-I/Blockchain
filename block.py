b1d6fffbf78d377ab313accd952be3e620fc45f6
Seledtsov Daniil <danikseledtsov@gmail.com>
2025-04-16 17:46:34 +0300

    Changed function check_integrity

--- block.py
+++ block.py
@@ -19,18 +19,29 @@ def get_hash(filename):
 
 def check_integrity():
     blockchain_dir = get_blockchain_dir()
-    files=get_sorted_files(blockchain_dir)
-    results=[]
-    for file in files[1:]:
-        h=json.load(open(blockchain_dir + str(file)))['hash']
-        prev_file=str(file-1)
-        actual_hash = get_hash(prev_file)
-        if h==actual_hash:
-            res='Ok'
+    files = get_sorted_files(blockchain_dir)
+    results = []
+    corruption_detected = False
+
+    for i in range(1, len(files)):
+        current_file = str(files[i])
+        previous_file = str(files[i - 1])
+
+        with open(blockchain_dir + current_file) as f:
+            current_data = json.load(f)
+
+        actual_prev_hash = get_hash(previous_file)
+
+        if corruption_detected or current_data['hash'] != actual_prev_hash:
+            res = 'Corrupted'
+            corruption_detected = True
         else:
-            res='Corrupted'
-        results.append({'block':prev_file,'result':res})
+            res = 'Everything is fine. No changes. 😊'
+
+        results.append({'block': previous_file, 'result': res})
+
     return results
+
 
 def write_block(name,amount,to_whom,prev_hash=''):
     blockchain_dir = get_blockchain_dir()
